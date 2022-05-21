26fc4f44cedb495b8f3a1a9606c3abdc6e6034c1
Vincent Rouillé <vincent@speedy37.fr>
2022-05-21 22:54:17 +0200

    Fix macos thread safety (#4)
    > The `getmntinfo()` function maintains ownership of the results buffer it allocates, and may overwrite or free this buffer in subsequent calls to `getmntinfo()`.  For this reason, `getmntinfo()` is not thread-safe.
    
    New implementation now uses `getfsstat$INODE64`

--- src/macos.rs
+++ src/macos.rs
@@ -53,7 +53,8 @@ struct statfs64 {
 }
 
 extern "C" {
-    fn getmntinfo64(mntbufp: *mut *const statfs64, flags: c_int) -> c_int;
+    #[link_name = "\u{1}_getfsstat$INODE64"]
+    fn getfsstat(buf: *mut statfs64, bufsize: c_int, flags: c_int) -> c_int;
 }
 
 #[derive(Debug)]
@@ -72,20 +73,23 @@ impl fmt::Display for Error {
 }
 
 fn _mounts(mut cb: impl FnMut(&statfs64, String) -> Result<(), Error>) -> Result<(), Error> {
-    let mut mntbuf: *const statfs64 = std::ptr::null_mut();
-    let mut n = unsafe { getmntinfo64(&mut mntbuf, MNT_NOWAIT) };
-    if n <= 0 {
+    let mut n: i32 = unsafe { getfsstat(std::ptr::null_mut(), 0, MNT_NOWAIT) };
+    let mut mntbuf = Vec::<statfs64>::new();
+    if n > 0 {
+        mntbuf.resize_with(n as usize, || unsafe { std::mem::zeroed() });
+        let bufsize = mntbuf.len() * std::mem::size_of::<statfs64>();
+        n = unsafe { getfsstat(mntbuf.as_mut_ptr(), bufsize as c_int, MNT_NOWAIT) };
+        if n >= 0 {
+            mntbuf.truncate(n as usize);
+        }
+    }
+    if n < 0 {
         return Err(Error::GetMntInfo64(unsafe { *libc::__error() }));
     }
-
-    while n > 0 {
-        let p: &statfs64 = unsafe { &*mntbuf };
+    for p in &mntbuf {
         let mountpath = unsafe { CStr::from_ptr(p.f_mntonname.as_ptr() as *const c_char) };
         cb(p, mountpath.to_str().map_err(|_| Error::Utf8Error)?.into())?;
-        mntbuf = unsafe { mntbuf.add(1) };
-        n -= 1;
     }
-
     Ok(())
 }
 
